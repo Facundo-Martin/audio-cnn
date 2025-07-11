cf285184b23248f8b760735eeac2f1274603595d
Facundo Martin <facundomartin98@gmail.com>
2025-07-11 12:30:59 -0300

    feat+fix: add endpoint data & fix remaining sample_rate

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import numpy as np
 import librosa
 import requests
 
-app = modal.App('adio-cnn-inference')
+app = modal.App('audio-cnn-inference')
 
 image = (modal.Image.debian_slim()
          .pip_install_from_requirements("requirements.txt")
@@ -24,14 +24,14 @@ class AudioProcessor:
     def __init__(self):
         self.transform = nn.Sequential(
             T.MelSpectrogram(
-            sample_rate=22050,
+            sample_rate=44100,
             n_fft=1024,
             hop_length=512,
             n_mels=128,
             f_min=0,
             f_max=11025
         ),
-        T.AmplitudeToDB()
+        T.AmplitudeToDB() 
         )
     
     def process_audio_chunk(self, audio_data):
@@ -69,34 +69,70 @@ class AudioClassifier:
     def inference(self, request: InferenceRequest):
         audio_bytes = base64.b64decode(request.audio_data)
 
+
         audio_data, sample_rate = sf.read(io.BytesIO(audio_bytes), dtype="float32")
 
         if audio_data.ndim > 1:
             audio_data = np.mean(audio_data, axis=1)
 
-        if sample_rate != 22050:
-            audio_data = librosa.resample(y=audio_data, orig_sr=sample_rate, target_sr=22050)
+        if sample_rate != 44100:
+            audio_data = librosa.resample(y=audio_data, orig_sr=sample_rate, target_sr=44100)
 
         spectogram = self.audio_processor.process_audio_chunk(audio_data)
         spectogram = spectogram.to(self.device)
 
         with torch.no_grad():
-            output = self.model(spectogram)
+            output, feature_maps = self.model(spectogram, return_feature_maps=True)
             output = torch.nan_to_num(output)
 
             probabilities = torch.softmax(output, dim=1)
             top3_probs, top3_indexes = torch.topk(probabilities[0], 3)
 
-            predictions = [{"class": self.classes[idx.item], "confidence": prob.item} for prob, idx in zip(top3_probs, top3_indexes)]
+            predictions = [{"class": self.classes[idx.item()], "confidence": prob.item()} for prob, idx in zip(top3_probs, top3_indexes)]
 
-        return {"predictions:": predictions}
+            viz_data = {}
+            for name, tensor in feature_maps.items():
+                if tensor.dim() == 4:
+                    aggregated_tensor = torch.mean(tensor, dim=1)
+                    squeezed_tensor = aggregated_tensor.squeeze(0)
+                    numpy_array = squeezed_tensor.cpu().numpy()
+                    clean_array = np.nan_to_num(numpy_array)
+                    viz_data[name] = {
+                        "shape": list(clean_array.shape),
+                        "values": clean_array.tolist(),
+                    }
+
+            spectogram_np = spectogram.squeeze(0).squeeze(0).cpu().numpy()
+            clean_speactogram = np.nan_to_num(spectogram_np)
+            input_spectogram = {
+                "shape": list(clean_speactogram.shape),
+                "values": clean_speactogram.tolist(),
+            }
+
+            max_samples = 8000
+            waveform_data = audio_data
+            if len(audio_data) > max_samples:
+                step = len(audio_data) // max_samples
+                waveform_data = audio_data[::step]
+        
+        return {
+            "predictions": predictions, 
+            "visualization": viz_data, 
+            "input_spectogram": input_spectogram, 
+            "waveform": {
+                "values": waveform_data.tolist(),
+                "sample_rate": 44100,
+                "duration": len(audio_data) / 44100
+            }
+        }
     
 
 @app.local_entrypoint()
 def main():
-    audio_data = sf.read('birds.wav')
+    audio_data, sample_rate = sf.read('birds.wav')
 
-    buffer = io.BytesIO(sf.write(buffer, audio_data, 22050, format="WAV"))
+    buffer = io.BytesIO()
+    sf.write(buffer, audio_data, sample_rate, format="WAV")
     audio_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
     payload = {"audio_data":  audio_b64}
 
@@ -106,6 +142,13 @@ def main():
     response.raise_for_status()
 
     result = response.json()
+
+    waveform_info = result.get("waveform", {})
+    if waveform_info:
+        values = waveform_info.get("values", {})
+        print(f"First 10 values: {[round(v, 4) for v in values[:10]]}...")
+        print(f"Duration: {waveform_info.get("duration", 0)}")
+
     print("Top predictions:")
     for pred in result.get("predictions", []):
         print(f' -{pred['class']} {pred['confidence']:0.2%}')
